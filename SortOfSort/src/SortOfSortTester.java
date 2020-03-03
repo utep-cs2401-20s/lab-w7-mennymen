fcb94c0b24dc75770810a56c4f3a974f312901dc
Manuel Melendez <mrmelendezl96@gmail.com>
2020-03-03 13:09:55 -0700

    Finished method and added comments Added test cases and added comments

--- SortOfSort/src/SortOfSortTester.java
+++ SortOfSort/src/SortOfSortTester.java
@@ -1,18 +1,50 @@
-
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class SortOfSortTester {
-    int[] test1 = {3,1,8,2,7};
 
+    //This test looks at an array with integers in no order, with some repeated values
+    //Test passed
     @Test
     public void sortOfSortTester1() {
-        int[] test1 = {3,1,-3,2,7};
-        int [] bla = sortOfSort.sortOfSort(test1);
-
-        for(int i = 0; i < bla.length;i++){
-            System.out.println(bla[i]);
+        int[] test1 = {7,6,1,9,5,3,7,0,4,3,1,8,7};
+        SortOfSort.sortOfSort(test1);
+        assertArrayEquals(test1, new int[] {7,7,5,4,1,1,0,3,3,6,7,8,9});
         }
 
+    //This test looks at an array of positive integers that is already sorted (not sort of sorted)
+    //Test passed
+    @Test
+    public void sortOfSortTester2(){
+        int[] test2 = {1,2,3,4,5,6,7};
+        SortOfSort.sortOfSort(test2);
+        assertArrayEquals(test2, new int[] {5,4,1,2,3,6,7});
     }
 
+    //This test looks at repeated numbers
+    //Test passed
+    @Test
+    public void sortOfSortTester3(){
+        int[] test3 = {1,4,1,4,1,4};
+        SortOfSort.sortOfSort(test3);
+        assertArrayEquals(test3, new int[] {4,1,1,1,4,4});
+    }
+
+    //This test looks at negative and positive numbers
+    //Test passed
+    @Test
+    public void sortOfSortTester4(){
+        int[] test4 = {-1,2,-3,4,0,-5,6,-7};
+        SortOfSort.sortOfSort(test4);
+        assertArrayEquals(test4, new int[] {2,0,-5,-7,-3,-1,4,6});
+    }
+
+    //This test only verifies that if the method receives an array that's already sort of sorted, it won't move anything
+    //Test passed
+    @Test
+    public void sortOfSortTester5(){
+        int[] test5 = {8,7,4,3,1,2,5,6,9,10};
+        SortOfSort.sortOfSort(test5);
+        assertArrayEquals(test5, new int[] {8,7,4,3,1,2,5,6,9,10});
+    }
 }
--- SortOfSort/src/sortOfSort.java
+++ SortOfSort/src/sortOfSort.java
@@ -1,45 +1,72 @@
-public class sortOfSort {
+public class SortOfSort {
 
-    public static int[] sortOfSort(int[] arr){
-        int counter = 0;
+    public static void sortOfSort(int[] arr){
+        //counter is used to know if the largest value is going at the beginning or the end of the array
+        int counter  = 0;
+
+        //start and end indexes are used to know the boundaries of the array as it is being sorted
         int startIndex = 0;
-        int endIndex = arr.length;
-        boolean lastIndex = true;
-        if(arr.length < 0)
-            return arr;
+        int endIndex = arr.length-1;
 
-        for(int i = 0; i < arr.length; i++){
-            if(lastIndex){
-                swapIndex(arr, findMax(arr, startIndex, endIndex), arr[arr.length-i-1]);
+        //max is the index of the maximum value in the array
+        int max = 0;
+
+        //this boolean is used to place the maximum value at the beginning or the end of the array
+        boolean lastIndex = true;
+
+        //this is kind of a base case, where the array only has one value or no values and there's nothing to sort
+        if(arr.length <= 1)
+            return;
+
+        for(int i = 0; i < arr.length; i++) {
+
+            //first we look for the maximum value in the array
+            max = findMax(arr, startIndex, endIndex);
+
+            //this if is for when the maximum value goes at the end of the array
+            if (lastIndex) {
+                swapIndex(arr, max, endIndex);
+                //endIndex decreases every time to know what is the upper boundary of what is left to be sorted
                 endIndex--;
             }
 
-            else{
-                swapIndex(arr, findMax(arr, startIndex, endIndex), arr[i]);
+            //this else if for when the maximum value goes at the beginning of the array
+            else {
+                swapIndex(arr, max, startIndex);
                 startIndex++;
             }
+
+            //counter always goes up with every iteration
             counter++;
 
-            if(counter == 2)
+            //when counter is larger than 2, it resets and changes the value of lastIndex to
+            //indicate the next largest value is going to be at the opposite end of the array
+            if (counter > 1) {
                 counter = 0;
                 lastIndex = !lastIndex;
+            }
         }
-        return arr;
     }
 
+    //Helped method to find the index of the maximum value in an array within the start and end indexes
+    //This method only returns the index of the maximum value as that's all we need for the sorting method, not the actual maximum value
     public static int findMax(int[] arr, int startIndex, int endIndex){
-        int max = Integer.MIN_VALUE;
-        for(int i = startIndex; i < endIndex; i++) {
-            if(arr[i] > max)
-                max = arr[i];
+        if(startIndex == endIndex)
+            return startIndex;
+        int[] max = {Integer.MIN_VALUE, -1};
+        for(int i = startIndex; i <= endIndex; i++){
+            if(arr[i] > max[0]){
+                max[0] = arr[i];
+                max[1] = i;
+            }
         }
-        return max;
+        return max[1];
     }
 
-    public static int swapIndex(int[] arr, int a, int b){
+    //Helper method to swap two values in an array
+    public static void swapIndex(int[] arr, int a, int b){
         int temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
     }
-
 }
